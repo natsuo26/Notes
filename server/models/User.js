3b07d13f51f2f7ce9a1d761697ea3b89a255244d
Abhay Kumar <abhaydkumar80@gmail.com>
2023-08-10 12:18:17 +0530

    initial update
    upload of the files

---
+++ server/models/User.js
@@ -0,0 +1,32 @@
+const mongoose=require('mongoose');
+
+const Schema=mongoose.Schema;
+const userSchema=new Schema({
+    googleId:   {
+        type:String,
+        required: true
+    },
+    displayName:    {
+        type:String,
+        required: true
+    },
+    firstName:    {
+        type:String,
+        required: true
+    },
+    lastName:    {
+        type:String,
+        required: true
+    },
+    profileImg:    {
+        type:String,
+        required: true
+    },
+    createdDate:    {
+        type:Date,
+        default:Date.now
+    }
+    
+});
+
+module.exports=mongoose.model('User',userSchema);
